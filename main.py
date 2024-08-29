e28f273c8be2f05a36c80f51834316c237137618
Ethan <ethan.ortiz.21@usjr.edu.ph>
2024-08-30 04:40:04 +0800

    idk

--- main.py
+++ main.py
@@ -1,161 +1,3 @@
-# import os
-# import re
-# import sys
-# import spacy
-# import json
-# from fastapi import FastAPI
-
-
-
-# app = FastAPI()
-
-# nlp = spacy.load('en_core_web_sm')  
-
-# def resource_path(relative):
-#     if hasattr(sys, "_MEIPASS"):
-#         return os.path.join(sys._MEIPASS, relative)
-#     return os.path.join(relative)
-
-# # Load the GI dictionary and lemma dictionary
-# GI_list = open(resource_path('data_files/inquirerbasic.txt'), 'r').readlines()
-# GI_dict = {}
-
-# for line in GI_list:
-#     entries = line.split("\t")  # Splits entries in .csv file at tab
-#     GI_dict[entries[0]] = set(entries[1:])
-
-# # Create dictionary for lemma list
-# lemma_list = open(resource_path('data_files/e_lemma_py_format_lower.txt'), 'r').readlines()
-# lemma_dict = {}
-
-# # Creates lemma dictionary, which may or may not be necessary with this database
-# for line in lemma_list:
-#     if line[0] == '#':
-#         continue
-#     entries = line.split()
-#     for word in entries:
-#         lemma_dict[word] = entries[0]
-
-# # Main function to process a list of texts
-# def process_text_list(text_list):
-#     result = []
-#     punctuation = (".", "!", "?", ",", ":", ";", "'", '"')
-
-#     for text in text_list:
-#         variable_list = []
-#         header_list = ["nwords"]
-
-#         text = re.sub("‘", "'", text)  # Bottom heavy educated quote replaced by straight quotes
-#         text = re.sub("’", "'", text)  # Top heavy educated quote replaced by straight quotes
-#         nwords = len(text.split())
-
-#         pre_text = text.lower().split()
-#         text_2 = []
-
-#         for word in pre_text:
-#             if len(word) < 1:
-#                 continue
-#             if len(word) == 1 and word in punctuation:
-#                 continue
-#             if word[-1] in punctuation:
-#                 word = word[:-1]
-#             if word[0] in punctuation:
-#                 word = word[1:]
-#             text_2.append(word)
-
-#         run_nrc(text_2, variable_list, header_list)
-
-#         result.append({
-#             "nwords": nwords,
-#             "data": variable_list,
-#             "headers": header_list
-#         })
-
-#     return json.dumps(result, indent=4)
-
-# # Function to calculate NRC scores
-# def run_nrc(text, variable_list, header_list, var=None):
-#     if var is None:
-#         pos = ""
-#     else:
-#         pos = var
-
-#     ListDict_counter(GI_dict, 'Anger_NRC', text, "yes", pos, variable_list, header_list)
-#     ListDict_counter(GI_dict, 'Anticipation_NRC', text, "yes", pos, variable_list, header_list)
-#     ListDict_counter(GI_dict, 'Disgust_NRC', text, "yes", pos, variable_list, header_list)
-#     ListDict_counter(GI_dict, 'Fear_NRC', text, "yes", pos, variable_list, header_list)
-#     ListDict_counter(GI_dict, 'Joy_NRC', text, "yes", pos, variable_list, header_list)
-#     ListDict_counter(GI_dict, 'Negative_NRC', text, "yes", pos, variable_list, header_list)
-#     ListDict_counter(GI_dict, 'Positive_NRC', text, "yes", pos, variable_list, header_list)
-#     ListDict_counter(GI_dict, 'Sadness_NRC', text, "yes", pos, variable_list, header_list)
-#     ListDict_counter(GI_dict, 'Surprise_NRC', text, "yes", pos, variable_list, header_list)
-#     ListDict_counter(GI_dict, 'Trust_NRC', text, "yes", pos, variable_list, header_list)
-
-# # Function to count occurrences of words in GI_dict and calculate their frequency
-# def ListDict_counter(list_dict, key, in_text, list_flag, pos, variable_list, header_list):
-#     counter = 0
-#     nwords = len(in_text)
-
-#     for word in in_text:
-#         if word in list_dict[key]:
-#             counter += 1
-#         else:
-#             if word in lemma_dict and lemma_dict[word] in list_dict[key]:
-#                 counter += 1
-
-#     if list_flag == "yes":
-#         variable_list.append(safe_divide(counter, nwords))
-#         if pos == "":
-#             if "NRC" in key:
-#                 key = key.replace("NRC", "EmoLex")
-#             header_list.append(key)
-#         else:
-#             if "NRC" in key:
-#                 key = key.replace("NRC", "EmoLex")
-#             header_list.append(key + pos)
-#     else:
-#         return safe_divide(counter, nwords)
-
-# # This function handles safe division to avoid division by zero
-# def safe_divide(numerator, denominator):
-#     if denominator == 0:
-#         index = 0
-#     else:
-#         index = numerator / denominator
-#     return index
-
-
-
-
-
-
-
-
-# @app.post("/Emotion-Analysis/")
-# async def process_texts(text_list: list[str]):
-#     output_json = process_text_list(text_list)
-#     return json.loads(output_json)
-
-# def test_process_text_list():
-#     example_text_list = [
-#         "I feel very stressed out.",
-#         "I feel happy and joyful today!",
-#         "This is a test sentence for processing."
-#     ]
-
-#     output_json = process_text_list(example_text_list)
-
-#     output_file_path = "output.json"
-
-#     with open(output_file_path, "w") as json_file:
-#         json_file.write(output_json)
-
-#     print("JSON fiel created")
-
-# # Call the function to test
-# test_process_text_list()
-
-
 import os
 import re
 import sys
@@ -164,8 +6,23 @@ import json
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # Your frontend application
+    # Add other origins if needed
+]
+
+# Add CORS middleware to allow requests from specified origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 nlp = spacy.load('en_core_web_sm')  
 
@@ -195,7 +52,7 @@ def process_text_list(text_list):
 
     for text in text_list:
         variable_list = []
-        header_list = ["nwords"]  # Initialize headers with word count
+        header_list = []  # Initialize headers with word count
 
         # Replace both types of smart quotes with straight quotes
         text = re.sub("‘|’", "'", text)  
@@ -239,7 +96,7 @@ def ListDict_counter(list_dict, key, in_text, variable_list, header_list):
     variable_list.append(safe_divide(counter, len(in_text)))
 
     if "NRC" in key:
-        key = key.replace("NRC", "EmoLex")
+        key = key.replace("_NRC", "")
     header_list.append(key)
 
 def safe_divide(numerator, denominator):
@@ -283,6 +140,6 @@ def test_process_text_list():
     print("JSON file created")
 
 
-test_process_text_list()
+# test_process_text_list()
 
 # *********************************************************************************************** #
